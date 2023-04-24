c63f7893ae3ae940f6a375fb908020b3b63366af
Bilal-Ahmed <bilal.ahmed@trenser.com>
2023-04-24 17:51:52 +0530

    add

---
+++ src/main/java/FunctionalInterfaceDemo.java
@@ -0,0 +1,26 @@
+@FunctionalInterface
+interface MyInterface {
+    // void print();
+
+    void sayHello();
+}
+
+public class FunctionalInterfaceDemo {
+    public static void main(String[] args) {
+        // MyInterface obj = new MyInterface() {
+
+        // @Override
+        // public void print() {
+        // System.out.println("Printing");
+        // throw new UnsupportedOperationException("Unimplemented method 'print'");
+        // }
+
+        // };
+        // obj.print();
+
+        MyInterface obj = () -> {
+            System.out.println("Hello, World!");
+        };
+        obj.sayHello();
+    }
+}
---
+++ src/main/java/LambdaDemo.java
@@ -0,0 +1,16 @@
+@FunctionalInterface
+interface Greeting {
+    void greet(String name);
+}
+
+public class LambdaDemo {
+    public static void main(String[] args) {
+        // Lambda expression as implementation of the greet method
+        Greeting greeting = (name) -> {
+            System.out.println("Hello, " + name + "!");
+        };
+
+        // Invoking the greet method using the lambda expression
+        greeting.greet("John"); // Output: Hello, John!
+    }
+}
