8e1f7d188733afa85bc4ebd968732b1d8ee67b60
BilalThaha <bilal.athaha@gmail.com>
2023-05-10 22:34:27 +0530

    ds

---
+++ src/main/java/datastructures/ArraysDemo.java
@@ -0,0 +1,24 @@
+package datastructures;
+
+import java.util.Arrays;
+
+public class ArraysDemo {
+    public static void main(String[] args) {
+        String[] colors = new String[5];
+        colors[0] = "purple";
+        colors[1] = "blue";
+
+        System.out.println("Array printing: ");
+        System.out.println(Arrays.toString(colors));
+
+        int[] numbers = { 100, 200 };
+
+        System.out.println("Array printing using enhanced for loop: ");
+        for (String col : colors) {
+            System.out.println(col);
+        }
+        System.out.println("Array printing using streams: ");
+        Arrays.stream(colors).forEach(System.out::println);
+    }
+
+}
