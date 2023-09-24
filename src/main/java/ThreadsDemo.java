512700c5abf10ff5a43eb73e3d64f7d74f163f1e
BilalThaha <bilal.athaha@gmail.com>
2023-09-24 08:22:34 +0530

    streams

---
+++ src/main/java/ThreadsDemo.java
@@ -0,0 +1,26 @@
+class A extends Thread {
+    public void show() {
+        for (int i = 0; i < 9; i++)
+            System.out.println("hi");
+    }
+}
+
+class B extends Thread {
+    public void show() {
+        for (int i = 0; i < 9; i++)
+            System.out.println("hello");
+    }
+}
+
+public class ThreadsDemo {
+    public static void main(String[] args) {
+
+        A obj1 = new A();
+        B obj2 = new B();
+
+        obj1.show();
+        obj2.show();
+
+    }
+
+}
